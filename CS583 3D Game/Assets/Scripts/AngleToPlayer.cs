c732b6a424596d8366d4d237626ea5609c8bd2ab
Alec Ege <aege1.1.2002@gmail.com>
2025-12-07 20:37:59 -0800

    Added Animations + Random Colors

--- CS583 3D Game/Assets/Scripts/AngleToPlayer.cs
+++ CS583 3D Game/Assets/Scripts/AngleToPlayer.cs
@@ -8,15 +8,16 @@ public class AngleToPlayer : MonoBehaviour
     private Vector3 targetPos;
     private Vector3 targetDir;
 
-    public SpriteRenderer spriteRenderer;
-    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+    //public Sprite[] sprites;
 
-    public float angle;
+    private float angle;
     public int lastIndex;
 
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>().transform;
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
@@ -25,6 +26,8 @@ public class AngleToPlayer : MonoBehaviour
         targetDir = targetPos - transform.position;
 
         angle = Vector3.SignedAngle(targetDir, transform.forward, Vector3.up);
+
+        //[!!!] Used to flip Sprite
 
         // Vector3 tempScale = Vector3.one;
         // if(angle > 0)
@@ -36,18 +39,18 @@ public class AngleToPlayer : MonoBehaviour
 
         lastIndex = GetIndex(angle);
 
-        spriteRenderer.sprite = sprites[lastIndex];
+        //spriteRenderer.sprite = sprites[lastIndex];
     }
 
     private int GetIndex(float angle)
     {
         if(angle >= -45f && angle <= 45f) //front
             return 0;
-        if(angle > -135f && angle < -45f) //right
+        if(angle > -135f && angle < -45f) //left
             return 1;
         if(angle <= -135f || angle >= 135f) //back
             return 2;
-        if(angle > 45f && angle < 135f) //left
+        if(angle > 45f && angle < 135f) //right
             return 3;
         
         return lastIndex;
